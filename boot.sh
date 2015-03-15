fa0c6e0ffe697df9412b2a4d7fde601f7abedb8a
Tuzki Zhang <rabbitzhang52@gmail.com>
2015-03-16 00:16:28 +0800

    nginx images

--- boot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-docker run -d\
-    -p 8888:80\
-    -v $(pwd)/etc/nginx/sites-available:/etc/nginx/sites-available\
-    -v $(pwd)/etc/nginx/sites-enabled:/etc/nginx/sites-enabled\
-    -v $(pwd)/var/htdocs:/var/htdocs\
-    -v $(pwd)/var/log/nginx:/var/log/nginx\
-    docker_ubuntu
---
+++ nginx/boot.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+docker run -d\
+    -p 8888:80\
+    -v $PWD/etc/nginx/sites-available:/etc/nginx/sites-available\
+    -v $PWD/etc/nginx/sites-enabled:/etc/nginx/sites-enabled\
+    -v $PWD/var/htdocs:/var/htdocs\
+    -v $PWD/var/log/nginx:/var/log/nginx\
+    nginx
