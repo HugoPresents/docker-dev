8defac7aea19e60c795ae012d8597c92a60f2d37
Tuzki Zhang <rabbitzhang52@gmail.com>
2016-06-02 09:11:58 +0800

    add ssh key

---
+++ images/php/5/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+if [ ! -d /root/.ssh ]
+then
+    mkdir /root/.ssh
+    chmod 700 /root/.ssh
+fi
+
+if [ -d /root/ssh-outside ]
+then
+    cp -rf /root/ssh-outside/* /root/.ssh
+    chmod 600 /root/.ssh/*
+fi
+
+exec "$@"
